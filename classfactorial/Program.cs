5f6aebc6090014e2c9b0c430b5ca6b54025ba143
ForánDavidDurango1725 <forand.d305@gmail.com>
2023-09-04 23:43:11 -0500

    Agregar archivos de proyecto.

---
+++ classfactorial/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classfactorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        Vusing System;
+
+        class ParImpar
+        {
+            static void Main()
+            {
+                int numero;
+
+                Console.Write("Ingrese un número: ");
+                numero = int.Parse(Console.ReadLine());
+
+                if (numero % 2 == 0)
+                {
+                    Console.WriteLine("El número es par");
+                }
+                else
+                {
+                    Console.WriteLine("El número es impar");
+                }
+            }
+        }
+    }
+}
